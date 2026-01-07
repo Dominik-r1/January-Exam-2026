d66daeca9b0f8d5cb78cb350803faebf37f1ddc3
Dominik Revtak <S00272657@atu.ie>
2026-01-07 10:06:22 +0000

    made abstract robot base class

---
+++ January Exam 2026/Robot.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace January_Exam_2026
+{
+    internal abstract class Robot
+    {
+        //properties
+        public string RobotName { get; set; }
+        public double PowerCapacityKWH { get; set; }
+        public double CurrentPowerKWH { get; set; }
+
+        //constructors
+
+        //methods
+        //returns battery level as a percentage
+        public double getBatteryPercentage()
+        {
+            // current battery / capacity X 100
+            return (CurrentPowerKWH / PowerCapacityKWH) * 100;
+        }
+
+        public string DisplayBatteryInformation()
+        {
+            return $"Battery Information\n" +
+                   $"Capacity: {PowerCapacityKWH} kWh\n" +
+                   $"Current Power: {CurrentPowerKWH} kWh\n" +
+                   $"Battery Level: {getBatteryPercentage()}%".ToString();
+        }
+        public abstract string DescribeRobot();
+     
+        public override string ToString()
+        {
+            return $"Robot Name: {RobotName} ]".ToString();
+        }
+    }
+}
