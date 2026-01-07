2eaf3879432473621ba894f2feccedc16e952a2e
Dominik Revtak <S00272657@atu.ie>
2026-01-07 10:19:44 +0000

    made household robot child class

---
+++ January Exam 2026/HouseholdRobot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace January_Exam_2026
+{
+    internal class HouseholdRobot : Robot
+    {
+        //properties
+        private List<HouseholdSkill>
+
+        //methods
+        public override string DescribeRobot()
+        {
+            //temporarily hardcoded
+            return $"I am a {RobotName} robot\n" +
+                $"I can help with chores aroung the house\n\n" +
+                $"{RobotName} Robot Skills:" +
+                $"-Skills list-\n\n" +
+                $"Battery Information:\n" +
+                $"{DisplayBatteryInformation()}".ToString();
+
+            
+        }
+    }
+}
